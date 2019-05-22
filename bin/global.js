1a328449e74a6f2459ed8db772180dbf65308954
Ankur Kushwaha <ankushwaha@expedia.com>
2019-05-22 09:59:19 +0530

    path fixed

--- bin/global.js
+++ bin/global.js
@@ -8,8 +8,8 @@ var app = express();
 var stubsServer=require('../index');
 var https = require('https');
 
-var privateKey = fs.readFileSync('../certificates/privatekey.pem').toString();
-var certificate = fs.readFileSync('../certificates/certificate.pem').toString();
+var privateKey = fs.readFileSync(path.join(__dirname,'../certificates/privatekey.pem')).toString();
+var certificate = fs.readFileSync(path.join(__dirname,'../certificates/certificate.pem')).toString();
 
 var options = {
   cert: certificate,
